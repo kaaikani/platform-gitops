c1311595d07fbf71e7bd518282ec9cbb1a03f5fb
haridass30 <haridass3014@gmail.com>
2026-07-17 14:48:02 +0530

    feat(terraform): bootstrap S3 backend + import Test-vpc

---
+++ terraform/version.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.11"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "kaaikani-tfstate-149536454380"
+    key          = "prod/terraform.tfstate"
+    region       = "ap-south-1"
+    profile      = "terraform-admin"
+    encrypt      = true
+    use_lockfile = true
+  }
+
+}
