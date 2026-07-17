4fe055fd0c1cc9563d1a61f0fe02a3fc41f04f2e
haridass30 <haridass3014@gmail.com>
2026-07-17 15:35:48 +0530

    feat(terraform): import vendure-production-vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,8 +1,17 @@
 resource "aws_vpc" "test" {
-  cidr_block           = "10.0.0.0/16"
+  assign_generated_ipv6_cidr_block     = false
+  cidr_block                           = "10.0.0.0/16"
+  enable_dns_hostnames                 = true
+  enable_dns_support                   = true
+  tags = {
+    Name = "Test-vpc"
+  }
+}
+resource "aws_vpc" "prod" {
+  cidr_block           = "10.10.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    Name = "Test-vpc"
+    Name = "vendure-production-vpc"
   }
 }
