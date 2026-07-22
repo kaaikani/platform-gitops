7218d7bfbb344d8c27c7a179f3ca276561d221fc
haridass30 <haridass3014@gmail.com>
2026-07-22 17:11:17 +0530

    feat(terraform): import standalone EC2s (Delivery-partner, GK_Construction)

---
+++ terraform/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "delivery_partner" {
+  ami = "ami-0f25d62448df9edee"
+  instance_type = "t3a.medium"
+  subnet_id = aws_subnet.public_1a.id
+  key_name = "new one"
+  ebs_optimized = true
+  monitoring = false
+  vpc_security_group_ids = [aws_security_group.eks_sg.id,aws_security_group.bastion.id,aws_security_group.eks_node_sg.id,aws_security_group.rds.id]
+  tags = {
+     ec2 = "delivery-partner", 
+     Name = "Delivery-partner" 
+  }
+}
+
+resource "aws_instance" "gk_const" {
+  ami = "ami-006f82a1d5a27da54"
+  instance_type = "t3a.medium"
+  subnet_id = aws_subnet.public_1a.id
+  key_name = "client_key_pair"
+  ebs_optimized = true
+  monitoring = true
+  vpc_security_group_ids = [
+    aws_security_group.eks_sg.id,
+    aws_security_group.eks_node_sg.id,
+    aws_security_group.rds.id,
+    "sg-0ba5c7c8f76401d4b",
+    "sg-08acbcb27549a1e75"
+    ]
+  tags = {
+    Name = "GK_Construction"
+  }
+}
